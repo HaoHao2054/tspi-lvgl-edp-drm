5662c653128d0e15d887f98d509619c620ae9195
yuanhao <2054190139@qq.com>
2025-06-07 00:06:41 +0800

    initial

---
+++ main.c
@@ -0,0 +1,119 @@
+#include "lvgl/demos/benchmark/lv_demo_benchmark.h"
+#include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
+#include "lv_drivers/display/drm.h"
+#include "lv_drivers/indev/evdev.h"
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include "gui_guider/generated/gui_guider.h"
+//#include "gui_guider/generated/events_init.h"
+
+#define DISP_BUF_SIZE (240 * 320)
+
+/*定义GUI GUIDER创建的UI结构体*/
+//lv_ui guider_ui;
+
+int main(void)
+{
+    /* 定义屏参 */
+    lv_coord_t lcd_w, lcd_h;
+
+    uint32_t lcd_dpi;
+
+    /*LittlevGL init*/
+    lv_init();
+
+    /*Linux frame buffer device init*/
+    // fbdev_init();
+    drm_init();
+    
+    drm_get_sizes(&lcd_w, &lcd_h, &lcd_dpi);
+    printf("LCD size: %dx%d, DPI: %d\n", lcd_w, lcd_h, lcd_dpi);
+
+    // /*A small buffer for LittlevGL to draw the screen's content*/
+    // static lv_color_t buf[DISP_BUF_SIZE];
+
+    /* 默认开启双缓冲 */
+    uint32_t draw_buf_size = lcd_w * lcd_h * sizeof(lv_color_t) / 4; /*1/4 screen sized buffer has the same performance */
+    static lv_disp_draw_buf_t disp_buf;
+    lv_color_t *buf_2_1 = malloc(draw_buf_size);
+    lv_color_t *buf_2_2 = malloc(draw_buf_size);
+    lv_disp_draw_buf_init(&disp_buf, buf_2_1, buf_2_2, draw_buf_size);
+
+
+    /*Initialize a descriptor for the buffer*/
+    // static lv_disp_draw_buf_t disp_buf;
+    // lv_disp_draw_buf_init(&disp_buf, buf, NULL, DISP_BUF_SIZE);
+
+    /*Initialize and register a display driver*/
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.draw_buf   = &disp_buf;
+    disp_drv.flush_cb   = drm_flush;
+    disp_drv.hor_res    = lcd_w;
+    disp_drv.ver_res    = lcd_h;
+    disp_drv.screen_transp = 1; /*Set to 1 if the screen has a transparent color (e.g. a color key)*/
+    /* 旋转屏幕 */
+    // disp_drv.sw_rotate  = 1;
+    // disp_drv.rotated    = LV_DISP_ROT_90;
+    lv_disp_drv_register(&disp_drv);
+
+    evdev_init();
+    static lv_indev_drv_t indev_drv_1;
+    lv_indev_drv_init(&indev_drv_1); /*Basic initialization*/
+    indev_drv_1.type = LV_INDEV_TYPE_POINTER;
+
+    /*This function will be called periodically (by the library) to get the mouse position and state*/
+    indev_drv_1.read_cb = evdev_read;
+    lv_indev_t *mouse_indev = lv_indev_drv_register(&indev_drv_1);
+
+#if 0
+
+    /*Set a cursor for the mouse*/
+    LV_IMG_DECLARE(mouse_cursor_icon)
+    lv_obj_t * cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
+    lv_img_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
+    lv_indev_set_cursor(mouse_indev, cursor_obj);             /*Connect the image  object to the driver*/
+#endif
+
+    /*Create a Demo*/
+    //lv_demo_widgets();
+    lv_demo_benchmark();
+    //lv_demo_music();
+    //lv_demo_widgets();
+    //lv_demo_keypad_encoder();
+
+    /*创建GUI GUIDER生成的UI*/
+    //setup_ui(&guider_ui);
+
+    /*Handle LitlevGL tasks (tickless mode)*/
+    while(1) {
+        lv_timer_handler();
+        usleep(5000);
+    }
+
+    return 0;
+}
+
+/*Set in lv_conf.h as `LV_TICK_CUSTOM_SYS_TIME_EXPR`*/
+uint32_t custom_tick_get(void)
+{
+    static uint64_t start_ms = 0;
+    if(start_ms == 0) {
+        struct timeval tv_start;
+        gettimeofday(&tv_start, NULL);
+        start_ms = (tv_start.tv_sec * 1000000 + tv_start.tv_usec) / 1000;
+    }
+
+    struct timeval tv_now;
+    gettimeofday(&tv_now, NULL);
+    uint64_t now_ms;
+    now_ms = (tv_now.tv_sec * 1000000 + tv_now.tv_usec) / 1000;
+
+    uint32_t time_ms = now_ms - start_ms;
+    return time_ms;
+}
---
+++ toolchain-aarch64-gnu.cmake
@@ -0,0 +1,26 @@
+# CMake toolchain file for arm-rockchip830-linux-uclibcgnueabihf
+
+# Target system
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# Cross compiler paths
+set(CROSS_COMPILER_PREFIX /home/hao/tools/toolchains/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-)
+
+set(CMAKE_C_COMPILER ${CROSS_COMPILER_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${CROSS_COMPILER_PREFIX}g++)
+
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+# Set the sysroot if necessary (often needed for includes and libraries)
+# Example: set(CMAKE_SYSROOT /home/hao/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/arm-rockchip830-linux-uclibcgnueabihf/sysroot)
+# If your toolchain handles sysroot implicitly, you might not need this.
+# Adjust the path above if your sysroot is located elsewhere within the toolchain directory.
+# set(CMAKE_SYSROOT /home/hao/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/arm-rockchip830-linux-uclibcgnueabihf/sysroot)
+
+# Configure find commands
+# set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+# set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+# set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
